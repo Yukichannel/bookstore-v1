00635e56d2a63d0d9ba86719c6480e7be0db6dfe
Baljir0901 <baljir0901@gmail.com>
2025-07-23 18:03:00 +0900

    README.md болон бүх кодын тайлбар, MERN төслийн эцсийн хувилбар

--- backend/controllers/bookController.js
+++ backend/controllers/bookController.js
@@ -31,8 +31,46 @@ const getAllBooks = async (req, res) => {
   }
 };
 
+//Ном засах updateBook controller функц
+const updateBook = async (req, res) => {
+  try {
+    const { id } = req.params; // URL-аас ID-г авах
+    const { title, author, year } = req.body; // Хүсэлтийн body-оос өгөгдөл авах
+
+    const updatedBook = await Book.findByIdAndUpdate(
+      id,
+      { title, author, year }, // Шинэ өгөгдөл
+      { new: true } // Шинэчилсэн номыг буцаах
+    );
+
+    if (!updatedBook) {
+      return res.status(404).json({ message: 'Ном олдсонгүй' });
+    }
+    res.status(200).json(updatedBook);
+  } catch (error) {
+    res.status(500).json({ message: 'Дотоод серверийн алдаа гарлаа' });
+  }
+};
+
+// Ном устгах deleteBook controller функц
+const deleteBook = async (req, res) => {
+  try {
+    const { id } = req.params; // URL-аас ID-г авах
+    const deletedBook = await Book.findByIdAndDelete(id); // Номыг устгах
+
+    if (!deletedBook) {
+      return res.status(404).json({ message: 'Ном олдсонгүй' });
+    }
+    res.status(200).json({ message: 'Ном амжилттай устгагдлаа' });
+  } catch (error) {
+    res.status(500).json({ message: 'Дотоод серверийн алдаа гарлаа' });
+  }
+};
+
 // Controller-уудыг экспортлох
 module.exports = {
   createBook,
   getAllBooks,
+  updateBook,
+  deleteBook,
 };
--- backend/routes/bookRoutes.js
+++ backend/routes/bookRoutes.js
@@ -3,12 +3,23 @@ const express = require('express');
 // Router үүсгэх
 const router = express.Router();
 // Controller-уудыг импортлох
-const { createBook, getAllBooks } = require('../controllers/bookController');
+const {
+  createBook,
+  getAllBooks,
+  deleteBook,
+  updateBook,
+} = require('../controllers/bookController');
 
 // POST /api/books - ном үүсгэх
 router.post('/', createBook);
 // GET /api/books - бүх номыг авах
 router.get('/', getAllBooks);
+
+// PUT /api/books/:id - ном засах
+router.put('/:id', updateBook);
+
+// DELETE /api/books/:id - ном устгах
+router.delete('/:id', deleteBook);
 
 // Router-ийг экспортлох
 module.exports = router;
