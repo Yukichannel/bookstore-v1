698987e5e5689e5431a8660bb401e6b5bfb73405
Yuki_channel <mikimongolia@gmail.com>
2025-08-04 21:10:37 +0800

    App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -21,7 +21,6 @@ function App() {
       setLoading(true);
       const response = await axios.get(`${API_URL}/api/books`);
       setbooks(Array.isArray(response.data) ? response.data : []);
-      setBooks(response.data);
     } catch (error) {
       console.error('Номын жагсаалтыг авахад алдаа гарлаа:', error);
     } finally {
